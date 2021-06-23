4c0fd7c0c001c1195c55648f2afa5048576d7dc9
Bertin <bertinmylene03@gmail.com>
2021-06-23 13:21:30 +0200

    Refactoring

--- Database.php
+++ Database.php
@@ -1,7 +1,7 @@
 <?php
 
 //Concernant la base de données...
-class Database
+abstract class Database
 {
 
     //Nos constantes
@@ -10,22 +10,65 @@ class Database
     const DB_PASS = '';
 
     /**
+     * Stocke la connexion si celle-ci existe, sinon renvoie  null
+     */
+    private $connection;
+
+
+    /**
+     * Teste si  $connection  est  null et appelle  getConnection()  pour créer une nouvelle connexion.
+     * Si  $connection  a une connexion existante, la méthode renvoie celle-ci.
+     */
+    private function checkConnection()
+    {
+        //Vérifie si la connexion est nulle et fait appel à getConnection()
+        if($this->connection === null) {
+            return $this->getConnection();
+        }
+        //Si la connexion existe, elle est renvoyée, inutile de refaire une connexion
+        return $this->connection;
+    }
+
+    /**
      * Se connecter à la base de données...
      * Grace à la méthode getConnection
      */
-    public function getConnection()
+    private function getConnection()
     {
         //Tentative de connexion à la base de données
         try{
-            $connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             //On renvoie un message avec le mot-clé return
-            return $connection;
+            return $this->connection;
         }
         //On lève une erreur si la connexion échoue
         catch(Exception $errorConnection)
         {
             die ('Erreur de connection :'.$errorConnection->getMessage());
         }
+    }
+
+    // Gére nos requêtes
+    /**
+     * @param mixed $sql requete sql
+     * @param null $parameters
+     * 
+     * @return [type]
+     */
+    protected function createQuery($sql, $parameters = null)
+    {
+        //Si tu as des parametres passés
+        if($parameters)
+        {
+            $result = $this->checkConnection()->prepare($sql);
+            $result->setFetchMode(PDO::FETCH_CLASS, static::class); //Fonction 'objet' plutôt que tableau
+            $result->execute($parameters);
+            return $result;
+        }
+        //Sinon
+        $result = $this->checkConnection()->query($sql);
+        $result->setFetchMode(PDO::FETCH_CLASS, static::class); //Fonction 'objet' plutôt que tableau
+        return $result;
     }
 }
--- Post.php
+++ Post.php
@@ -1,29 +1,20 @@
 <?php
 
 //Concernant les articles...
-class Post
+class Post extends Database
 {
     //Récupére tous les articles
     public function getPosts()
     {
-        $db = new Database();
-        $connection = $db->getConnection();
-
-        $result = $connection->query('SELECT id, title, content, author, createdAt FROM post ORDER BY id DESC');
-        return $result;
+        $sql = 'SELECT id, title, content, author, createdAt FROM post ORDER BY id DESC';
+        return $this->createQuery($sql);
     }
 
     //Récupére un article en fonction de l'id transmis
     public function getPost($postId)
     {
-        $db = new Database();
-        $connection = $db->getConnection();
-
-        $result = $connection->prepare('SELECT id, title, content, author, createdAt FROM post WHERE id = ?');
-        $result->execute([
-            $postId
-        ]);
-        return $result;
+        $sql = 'SELECT id, title, content, author, createdAt FROM post WHERE id = ?';
+        return $this->createQuery($sql, [$postId]);
     }
 
 
