4675fecc9cde90453b9017408b987064c269dd08
Bertin <bertinmylene03@gmail.com>
2021-06-30 16:36:37 +0200

    Suppression d'article et commentaires associés

--- templates/form_post.php
+++ templates/form_post.php
@@ -13,14 +13,20 @@ $route = isset($post) && $post->get('id') ? 'editPost&postId=' . $post->get('id'
 $submit = $route === 'addPost' ? 'Envoyer' : 'Mettre à jour';
 $title = isset($post) ? htmlspecialchars($post->get('title')) : '';
 $content = isset($post) ? htmlspecialchars($post->get('content')) : '';
+$author = isset($post) ? htmlspecialchars($post->get('author')) : '';
 
 ?>
-<form action="../public/index.php?route=<?= $route; ?>" method="post">
+<form method="post" action="../public/index.php?route=<?= $route; ?>">
     <label for="title">Titre</label><br>
     <input type="text" id="title" name="title" value="<?= $title; ?>"><br>
     <?= isset($errors['title']) ? $errors['title'] : '' ?>
+
     <label for="content">Contenu</label><br>
     <textarea name="content" id="contenu" cols="30" rows="10"><?= $content; ?></textarea><br>
     <?= isset($errors['content']) ? $errors['content'] : '' ?>
+
+    <label for="author">Auteur</label><br>
+    <input type="text" id="author" name="author" value="<?= $author; ?>"><br><br>
+
     <input type="submit" value="<?= $submit; ?>" id="submit" name="submit">
 </form>
